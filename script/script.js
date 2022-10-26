d090099e1c382546fb0855df7dd40ee09fe63458
AdrianBottini <adrianboh@gmail.com>
2022-10-26 16:26:23 +0200

    js+css

---
+++ script/script.js
@@ -0,0 +1,48 @@
+const arrImages = [
+	'img/01.jpg',
+	'img/02.jpg',
+	'img/03.jpg',
+	'img/04.jpg',
+	'img/05.jpg',
+];
+
+const eleSlider = document.querySelector('.slider');
+const eleBtnLeft = document.querySelector('.btn-up');
+const eleBtnRight = document.querySelector('.btn-down');
+
+for (let i = 0; i < arrImages.length; i++) {
+	const eleImg = document.createElement('img');
+	eleImg.src = arrImages[i];
+	eleImg.classList.add('slider-img');
+
+	if (i === 0) {
+		eleImg.classList.add('active');
+	}
+
+	eleSlider.append(eleImg);
+}
+
+const listEleImg = document.querySelectorAll('.slider-img'); 
+let activeIndex = 0;
+
+eleBtnRight.addEventListener('click', function () {
+	listEleImg[activeIndex].classList.remove('active');
+	activeIndex++;
+	listEleImg[activeIndex].classList.add('active');
+
+	eleBtnLeft.classList.remove('hidden');
+	if (activeIndex === listEleImg.length - 1) {
+		eleBtnRight.classList.add('hidden');
+	}
+});
+
+eleBtnLeft.addEventListener('click', function () {
+	listEleImg[activeIndex].classList.remove('active');
+	activeIndex--;
+	listEleImg[activeIndex].classList.add('active');
+
+	eleBtnRight.classList.remove('hidden');
+	if (activeIndex === 0) {
+		eleBtnLeft.classList.add('hidden');
+	}
+});
